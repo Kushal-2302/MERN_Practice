ccdd447b2f571afc7c440617cb81b3b94267d277
Kushal S <kushals0223@gmail.com>
2026-04-03 12:49:54 +0530

    added

--- web_revision/promise/app.js
+++ web_revision/promise/app.js
@@ -1,61 +1,61 @@
 //! ---------Fecthing Data with Promises
-// fetch("https://jsonplaceholder.typicode.com/users")
-// .then(response => response.json())
-// .then(data => {
-//     console.log("Users fetched successfully: ", data);
-// })
-// .catch(error => {
-//     console.error("Error fetching users: ",error)
-// })
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(response => response.json())
+.then(data => {
+    console.log("Users fetched successfully: ", data);
+})
+.catch(error => {
+    console.error("Error fetching users: ",error)
+})
 
 
 //! ---------Chaning Promises
-// fetch("https://jsonplaceholder.typicode.com/users")
-// .then(response => response.json())
-// .then(users => {
-//     console.log("Total users: ", users.length);
-//     return users[0];
-// })
-// .then(firstUser => {
-//     console.log("firstUser: ",firstUser);
-// })
-// .catch(error => {
-//     console.log("Users not found: ",error);
-// })
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(response => response.json())
+.then(users => {
+    console.log("Total users: ", users.length);
+    return users[0];
+})
+.then(firstUser => {
+    console.log("firstUser: ",firstUser);
+})
+.catch(error => {
+    console.log("Users not found: ",error);
+})
 
 
 //! --------Fetch data sing async/await
-// async function getUsers(){
-//     try {
-//         const response = await fetch("https://jsonplaceholder.typicode.com/users");
-//         const users = await response.json();
-//         console.log("Users fetched successfully: ",users);
-//         users.forEach((user) => console.log(user.name));
-//     }catch(error){
-//         console.error("Error fetching users: ",error);
-//     }
-// }
-// getUsers()
+async function getUsers(){
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const users = await response.json();
+        console.log("Users fetched successfully: ",users);
+        users.forEach((user) => console.log(user.name));
+    }catch(error){
+        console.error("Error fetching users: ",error);
+    }
+}
+getUsers()
 
 
 //! ------User Search
-// async function findUserByName(name){
-//     try {
-//         const response  = await fetch("https://jsonplaceholder.typicode.com/users");
-//         const users = await response .json();
-//         const user = users.find(u => u.name.toLowerCase() === name.toLowerCase());
+async function findUserByName(name){
+    try {
+        const response  = await fetch("https://jsonplaceholder.typicode.com/users");
+        const users = await response .json();
+        const user = users.find(u => u.name.toLowerCase() === name.toLowerCase());
 
-//         if(user){
-//             console.log("User found: ",user);
-//         }else{
-//             console.log(" User not  found.")
-//         }
-//     }catch(error){
-//         console.log("Error fetching users: ",error);
-//     }
-// }
-// findUserByName("Kushal Kumar"); //User not found
-// findUserByName("Leanne Graham"); // Data will be displayed
+        if(user){
+            console.log("User found: ",user);
+        }else{
+            console.log(" User not  found.")
+        }
+    }catch(error){
+        console.log("Error fetching users: ",error);
+    }
+}
+findUserByName("Kushal Kumar"); //User not found
+findUserByName("Leanne Graham"); // Data will be displayed
 
 
 //! -------email & password check using promise
